5fed5ee3baccbd29697b3ec2dcd27befc701d64e
Jesse <catsjc@hotmail.com>
2026-03-30 13:00:32 -0700

    Create Landing.js with login button and intro

---
+++ frontend/src/pages/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div>
+            <h1>Welcome to the Trading Card App</h1>
+            <p>Your introduction to the amazing world of trading cards!</p>
+            <button onClick={() => alert('Login functionality to be implemented!')}>Login</button>
+        </div>
+    );
+};
+
+export default Landing;
